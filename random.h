cb673703c511e2b983eef373e221ab1763d9b416
Robert D. Vincent <robert.d.vincent@mcgill.ca>
2014-10-01 14:47:44 -0400

    New random.h

---
+++ random.h
@@ -0,0 +1,9 @@
+/**
+ * \file
+ * \brief Random number generation.
+ *
+ * Copyright (c) 2008-2014 Robert D. Vincent.
+ */
+double rnd1();
+double rndInterval(double minv, double maxv);
+double rndNorm();
